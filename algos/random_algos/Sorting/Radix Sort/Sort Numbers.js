2eab9c08d1802c48da3096fe3d51c46c64fee732
linh4 <linh44h@gmail.com>
2019-01-05 21:32:52 -0500

    radix sort

---
+++ algos/random_algos/Sorting/Radix Sort/Sort Numbers.js
@@ -0,0 +1,34 @@
+// Sort the array of numbers in order.
+
+function getDigit(num, i) {
+  return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10
+}
+
+function digitCount(num) {
+  if (num === 0) return 1
+  return Math.floor(Math.log10(Math.abs(num))) + 1
+}
+
+function mostDigit(nums) {
+  let maxDigits = 0
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(nums[i]))
+  }
+  return maxDigits
+}
+
+function radixSort(nums) {
+  let maxDigitCount = mostDigit(nums)
+  for (let k = 0; k < maxDigitCount; k++) {
+    let digitBuckets = Array.from({length: 10}, () => [])
+    for (let i = 0; i < nums.length; i++) {
+      let digit= getDigit(nums[i], k)
+      digitBuckets[digit].push(nums[i])
+    }
+    nums = [].concat(...digitBuckets)
+  }
+  return nums
+}
+
+// mostDigit([23,54,899,122445,9])
+radixSort([234, 3525, 12, 455, 2, 56, 9824])
